b0e157c22c58fec13dd02a45f4c300d6c31b9363
Samuelyihao <samuel0415yihao@gmail.com>
2020-05-29 16:50:06 +0800

    此次commit如下: 1. 新增 django-import-export 建置一鍵上下傳 2. 更新 requirements 內容 3. 更新 admin 內容 4. 更新 model 內容 5. TODO: search_news 待修改新增內容

--- app_news_summary/models.py
+++ app_news_summary/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 
-class summary_crawl(models.Model):
-    title    = models.CharField(max_length=50)
-    content  = models.TextField()
-    time     = models.DateTimeField()
-    url      = models.URLField()
-    topic    = models.CharField(max_length=10)
-    jeiba    = models.TextField()
-    summary1 = models.TextField()
+# class summary_crawl(models.Model):
+#     title    = models.CharField(max_length=50)
+#     content  = models.TextField()
+#     time     = models.DateTimeField()
+#     url      = models.URLField()
+#     topic    = models.CharField(max_length=10)
+#     jeiba    = models.TextField()
+#     summary1 = models.TextField()
+
+class SummaryCrawl(models.Model):
+	title = models.CharField(max_length=50)
+	content = models.TextField()
+	date = models.DateField()
+	url = models.URLField()
+	topic = models.CharField(max_length=10)
+	jeiba = models.TextField()
+	summary1 = models.TextField()
+
+class CrawlNews(models.Model):
+	# sub_title, sub_content 選填
+	title = models.CharField(max_length=100)
+	sub_title = models.CharField(max_length=100,blank=True)
+	content = models.TextField()
+	sub_content = models.TextField(blank=True)
+	date = models.DateField(blank=True)
+	# date = models.CharField(max_length=20)
+	url = models.URLField()
---
+++ app_news_summary/static/js/custom_style.js
@@ -0,0 +1,31 @@
+const checkbox = document.getElementById("daynightswitch");
+const body = document.getElementsByTagName("body")[0];
+const main = document.getElementsByTagName("main")[0];
+
+const table = document.getElementsByTagName("table");
+const thead = document.getElementsByTagName("thead");
+var logo_bw = document.getElementById("logo");
+
+checkbox.addEventListener("change", function(){
+  body.classList.toggle("day");
+  main.classList.toggle("day");
+
+  for (var i = 0; i < table.length; i++) {
+    if (table[i].style.color == "rgb(255, 255, 255)") {
+      table[i].style.color = "#212529";
+      thead[i].style.color = "#212529";
+    } else if (table[i].style.color == "rgb(33, 37, 41)") {
+      table[i].style.color = "#ffffff";
+      thead[i].style.color = "#ffffff";
+    } else {
+      table[i].style.color = "#212529";
+      thead[i].style.color = "#212529";
+    }
+  };
+
+  if ( logo_bw.src.indexOf("ss_logo_word-white.png") > -1) {
+    logo_bw.src = "static/images/ss_logo.png"
+  } else {
+    logo_bw.src = "static/images/ss_logo_word-white.png"
+  };
+});

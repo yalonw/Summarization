532376173409fdcff43b3a9b1c545a99c96b0124
JUE <yalonw@gmail.com>
2020-06-04 01:48:40 +0800

    Add new app : text summary

--- app_text_summary/urls.py
+++ app_text_summary/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('analyze-result', views.summary_esbert, name='analyze_article'),
 ]

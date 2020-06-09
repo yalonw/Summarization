7b5349cb60ea0d0dcf23f5fd6d0dfb6ad3531cd7
JUE <yalonw@gmail.com>
2020-06-09 23:12:11 +0800

    Add new function: extract time event

--- app_text_summary/views.py
+++ app_text_summary/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from project_module import extractive_summarization_bert as esbert
+from project_module import extract_time as extime
 
 def homepage(request):
     return render(request, 'index2.html', locals())
@@ -8,4 +9,6 @@ def summary_esbert(request):
     article = request.POST['article']
     summary_num = int(request.POST['summary_num'])
     summary = esbert.Bert_extractive(article, summary_num).make_summary()
+    mark = ['，', '”', '」', ')']
+    extract_time = extime.GetEvent.time_event(article, mark)
     return render(request, 'analyze_result.html', locals())

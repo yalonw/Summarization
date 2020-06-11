0958dd6e3ec742c206bd6ecaaa0f131e9780f7e9
samuel <samuel0415yihao@gmail.com>
2020-06-11 15:17:44 +0800

    文字雲實裝。

--- app_news_summary/admin.py
+++ app_news_summary/admin.py
@@ -4,7 +4,8 @@ from import_export.admin import ImportExportModelAdmin
 
 
 # Register your models here.
-class SummaryCrawlAdmin(admin.ModelAdmin):
+@admin.register(SummaryCrawl)
+class SummaryCrawlAdmin(ImportExportModelAdmin):
 	# 加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
 	list_display=('id','title','content','date','url','topic','jeiba','summary1')
 	list_filter=('date','topic')
@@ -17,13 +18,3 @@ class CrawlNewsAdmin(ImportExportModelAdmin):
 	list_filter=('date',)
 	serch_fields=('title',)
 	ordering=('id',)
-
-# class CrawlNewsAdmin(admin.ModelAdmin):
-# 	list_display=('id','title','content','time','url')
-# 	list_filter=('time',)
-# 	serch_fields=('title',)
-# 	ordering=('id',)
-
-
-admin.site.register(SummaryCrawl,SummaryCrawlAdmin)
-# admin.site.register(CrawlNews,CrawlNewsAdmin)
--- app_news_summary/views.py
+++ app_news_summary/views.py
@@ -7,124 +7,151 @@ import pandas as pd
 from project_module import extractive_summarization_jieba as esjieba
 import os
 from project_module import word_cloud
+import csv
+
 
 def homepage(request):
-	'''首頁'''
-	now = datetime.now()
-	return render(request, 'index.html', locals())
+    '''首頁'''
+    now = datetime.now()
+    return render(request, 'index.html', locals())
 
 
 def summary_esjieba_crawl(request):
-	'''透過即時爬蟲取得資料，再進行新聞摘要'''
+    '''透過即時爬蟲取得資料，再進行新聞摘要'''
 
-	# 取得輸入之關鍵字、頁數，開始爬蟲
-	keyword = request.POST['keyword']
-	# num_of_news = request.POST['num_of_news']
-	num_of_news = 11
+    # 取得輸入之關鍵字、頁數，開始爬蟲
+    keyword = request.POST['keyword']
+    # num_of_news = request.POST['num_of_news']
+    num_of_news = 11
 
-	print("keyword: ", keyword, "\n""views: num_of_news: ", num_of_news)
-	df_news = cn.Crawl_NYtimes(keyword, int(num_of_news)).make_dataframe()
+    print("keyword: ", keyword, "\n""views: num_of_news: ", num_of_news)
+    df_news = cn.Crawl_NYtimes(keyword, int(num_of_news)).make_dataframe()
 
-	# 取得爬蟲資料後，做LDA模型分類
-	lda = ltm.LDAclass(df_news, chinese_only=True)  # 只做 jieba 分詞
-	lda.lda_class(n_topics=3, max_iter=100,
-				  evaluate_every=10, verbose=1)  # 做 LDA
-	df_lda = lda.dataframe
+    # 取得爬蟲資料後，做LDA模型分類
+    lda = ltm.LDAclass(df_news, chinese_only=True)  # 只做 jieba 分詞
+    lda.lda_class(n_topics=3, max_iter=100,
+                  evaluate_every=10, verbose=1)  # 做 LDA
+    df_lda = lda.dataframe
 
-	# 取得LDA結果，做抽取式摘要
-	summary = esjieba.Drawable_summary(
-		df_lda, 'text_rank', 25, '2', keyword).make_summary()
+    # 取得LDA結果，做抽取式摘要
+    summary = esjieba.Drawable_summary(
+        df_lda, 'text_rank', 25, '2', keyword).make_summary()
 
-	return render(request, 'search_result.html', locals())
+    return render(request, 'search_result.html', locals())
 
 
 def summary_esjieba_database(request):
-	'''透過既存資料庫取得資料，再進行新聞摘要'''
+    '''透過既存資料庫取得資料，再進行新聞摘要'''
 
-	# 取得輸入之關鍵字、日期期間
-	keyword = request.POST['keyword']
-	search_time = request.POST['search-time']
-	date_now = date.today()
+    # 取得輸入之關鍵字、日期期間
+    keyword = request.POST['keyword']
+    search_time = request.POST['search-time']
+    date_now = date.today()
 
-	# 先針對contains搜尋, 並依時間降序排列
-	db_news = models.CrawlNews.objects.filter(
-		content__contains=keyword
-	).order_by('id')
+    # 先針對contains搜尋, 並依時間降序排列
+    db_news = models.CrawlNews.objects.filter(
+        content__contains=keyword
+    ).order_by('id')
 
-	# 再依選擇的時間區間再次縮小搜尋範圍
-	if search_time == "30":
-		print("="*20+"過去一個月"+"="*20)
-		date_end = timedelta(days=30)
-		D1 = date_now-date_end
-		db_news_day = db_news.filter(
-			date__gte=D1
-		).order_by('id')
+    # 再依選擇的時間區間再次縮小搜尋範圍
+    if search_time == "30":
+        print("="*20+"過去一個月"+"="*20)
+        date_end = timedelta(days=30)
+        D1 = date_now-date_end
+        db_news_day = db_news.filter(
+            date__gte=D1
+        ).order_by('id')
 
-	elif search_time == "90":
-		print("="*20+"過去三個月"+"="*20)
-		date_end = timedelta(days=90)
-		D1 = date_now-date_end
-		db_news_day = db_news.filter(
-			date__gte=D1
-		).order_by('id')
+    elif search_time == "90":
+        print("="*20+"過去三個月"+"="*20)
+        date_end = timedelta(days=90)
+        D1 = date_now-date_end
+        db_news_day = db_news.filter(
+            date__gte=D1
+        ).order_by('id')
 
-	elif search_time == "180":
-		print("="*20+"過去半年"+"="*20)
-		date_end = timedelta(days=180)
-		D1 = date_now-date_end
-		db_news_day = db_news.filter(
-			date__gte=D1
-		).order_by('id')
+    elif search_time == "180":
+        print("="*20+"過去半年"+"="*20)
+        date_end = timedelta(days=180)
+        D1 = date_now-date_end
+        db_news_day = db_news.filter(
+            date__gte=D1
+        ).order_by('id')
 
-	elif search_time == "360":
-		print("="*20+"過去一年"+"="*20)
-		date_end = timedelta(days=360)
-		D1 = date_now-date_end
-		db_news_day = db_news.filter(
-			date__gte=D1
-		).order_by('id')
-	else:
-		print("="*20+"全部紀錄"+"="*20)
-		db_news_day = db_news
+    elif search_time == "360":
+        print("="*20+"過去一年"+"="*20)
+        date_end = timedelta(days=360)
+        D1 = date_now-date_end
+        db_news_day = db_news.filter(
+            date__gte=D1
+        ).order_by('id')
+    else:
+        print("="*20+"全部紀錄"+"="*20)
+        db_news_day = db_news
 
-	# 將搜尋完結果存成dataframe
-	datesize = len(db_news_day)
-	df_news = pd.DataFrame(list(db_news_day.values(
-		"title", "sub_title", "content", "sub_content", "date", "url"
-	)))
+    # 將搜尋完結果存成dataframe
+    datesize = len(db_news_day)
+    df_news = pd.DataFrame(list(db_news_day.values(
+        "title", "sub_title", "content", "sub_content", "date", "url"
+    )))
 
-	try:
-		# 取得資料庫資料後，做LDA模型分類
-		lda = ltm.LDAclass(df_news, chinese_only=True,
-						   stop_words="app_news_summary/static/stop_words.txt")  # 只做 jieba 分詞
-		lda.lda_class(n_topics=3, max_iter=100,
-					  evaluate_every=10, verbose=1)  # 做 LDA
-		df_lda = lda.dataframe
+    try:
+        # 取得資料庫資料後，做LDA模型分類
+        lda = ltm.LDAclass(df_news, chinese_only=True,
+                           stop_words="app_news_summary/static/stop_words.txt")  # 只做 jieba 分詞
+        lda.lda_class(n_topics=3, max_iter=100,
+                      evaluate_every=10, verbose=1)  # 做 LDA
+        df_lda = lda.dataframe
 
-		# 取得LDA結果，做抽取式摘要
-		summary = esjieba.Drawable_summary(
-			df_lda, 'text_rank', 25, '2', keyword).make_summary()
+        if keyword == "肺炎" and search_time == "180":
+            # 展示範利用，取得已分析好的結果摘要，直接呈現在網頁上
+            # 範例: keyword=肺炎, search_time=過去半年
+            date_now = date.today()
+            date_end = timedelta(days=180)
+            D1 = date_now-date_end
+            summary_for_database = models.SummaryCrawl.objects.filter(
+                content__contains=keyword).filter(
+                date__gte=D1).order_by(
+                'id')
+            summary = pd.DataFrame(list(summary_for_database.values(
+                "title", "content", "date", "url", "topic", "jeiba", "summary1"
+            )))
+        elif keyword == "中國" and search_time == "all":
+            # 展示範利用，取得已分析好的結果摘要，直接呈現在網頁上
+            # 範例: keyword=中國, search_time=全部紀錄
+            summary_for_database = models.SummaryCrawl.objects.filter(
+                content__contains=keyword).order_by('id')
+            summary = pd.DataFrame(list(summary_for_database.values(
+                "title", "content", "date", "url", "topic", "jeiba", "summary1"
+            )))
+        else:
+            # 取得LDA結果，做抽取式摘要
+            summary = esjieba.Drawable_summary(
+                df_lda, 'text_rank', 25, '2', keyword).make_summary()
 
-		# 文字雲製作
-		for i in range(0, 3):
-			try:
-				df = df_lda.groupby('topic').get_group(i)
-				df_1 = df['jieba'].sum()
-				wc = word_cloud.wc_1(df_1)
-				wc = wc.to_file(
-					'app_news_summary/static/images/wordcloud'+str(i)+'.png')
-			except KeyError:
-				pass
-	except KeyError:
-		pass
+        # 輸出csv檔
+        summary.to_csv("app_news_summary/static/summary_output_"+str(keyword)+"_"+str(search_time)+".csv")
 
-	# 如果查詢資料庫無資料，要刪掉之前已產生的文字雲圖片
-	if datesize == 0:
-		for i in range(0, 3):
-			try:
-				os.remove(
-					'app_news_summary/static/images/wordcloud'+str(i)+'.png')
-			except OSError as e:
-				print(e)
+        # 文字雲製作
+        for i in range(0, 3):
+            try:
+                df = df_lda.groupby('topic').get_group(i)
+                df_1 = df['jieba'].sum()
+                wc = word_cloud.wc_1(df_1)
+                wc = wc.to_file(
+                    'app_news_summary/static/images/wordcloud'+str(i)+'.png')
+            except KeyError:
+                pass
+    except KeyError:
+        pass
 
-	return render(request, 'search_result.html', locals())
+    # 如果查詢資料庫無資料，要刪掉之前已產生的文字雲圖片
+    if datesize == 0:
+        for i in range(0, 3):
+            try:
+                os.remove(
+                    'app_news_summary/static/images/wordcloud'+str(i)+'.png')
+            except OSError as e:
+                print(e)
+
+    return render(request, 'search_result.html', locals())

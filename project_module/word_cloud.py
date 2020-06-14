92f986937ccd720e1324cad108b4bbd5f147a0a3
JUE <yalonw@gmail.com>
2020-06-15 01:06:54 +0800

    Add stopwords to WordCloud

--- project_module/word_cloud.py
+++ project_module/word_cloud.py
@@ -1,19 +1,23 @@
 from wordcloud import WordCloud
 import numpy as np
-# 文字雲
 
 def wc_1(text):
-	
-	stopwords='app_news_summary/static/stop_words.txt'
-	diction = text
-	x, y = np.ogrid[:300, :300]
-	mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
-	mask = 255 * mask.astype(int)
-	font = 'app_news_summary/static/fonts/NotoSansTC-Regular.otf'
-	
-	wc = WordCloud(background_color='black',
-				   font_path=font,
-				   mask=mask,
-				   stopwords=stopwords
-				   ).generate(diction)
-	return wc
+    '文字雲'
+    diction = text
+    x, y = np.ogrid[:500, :500]
+    mask = (x-250)**2 + (y-250)** 2 > 240 ** 2
+    mask = 255 * mask.astype(int)
+    font = 'app_news_summary/static/fonts/NotoSansTC-Regular.otf'
+
+    stopwords = set()
+    stopwords_filepath = 'app_news_summary/static/stop_words.txt'
+    with open(stopwords_filepath, 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            stopwords.add(line.strip('\n'))
+
+    wc = WordCloud(background_color='white',
+                   font_path=font,
+                   mask=mask,
+                   stopwords=stopwords
+                   ).generate(diction)
+    return wc
